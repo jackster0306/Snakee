985c14588d95812b12d44af2a78d0e8fb2ce7f41
psyjg14 <psyjg14@nottingham.ac.uk>
2022-11-25 15:53:59 +0000

    chore: Created some new function for duplicate code
    Any code that was often duplicated, I created a function for.
    
    For example, there was a few times where the code created bounds and then checked if the snake head intersected with these bounds. I put this into a function CheckBounds where a Node is sent and the function checks if the snakehead intersects with the given Node.

--- src/Application/PlayScreenController.java
+++ src/Application/PlayScreenController.java
@@ -4,6 +4,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -59,8 +60,6 @@ public class PlayScreenController {
 
     Wall wall;
 
-    boolean wallhit = false;
-
 
     public static String GetScore() {
         return Integer.toString(score);
@@ -78,7 +77,9 @@ public class PlayScreenController {
 
     Timeline walltl;
 
-    int hit = 0;
+    boolean hit = false;
+
+    boolean intersects;
 
     public void initialize(){
         wallticks = 0;
@@ -87,17 +88,11 @@ public class PlayScreenController {
         isbombs = StartScreenController.GetToBomb();
         difficulty = StartScreenController.GetDiff();
         if(difficulty == 1){
-            bombspawn = 14;
-            xbound = 870;
-            ybound = 560;
+            SetVariables(870,560,14);
         } else if (difficulty == 2) {
-            bombspawn = 9;
-            xbound = 550;
-            ybound = 400;
+            SetVariables(550,400,9);
         } else if (difficulty == 3) {
-            bombspawn = 5;
-            xbound = 420;
-            ybound = 280;
+            SetVariables(420,280,5);
         }
         bomb = new Bomb(rand.nextInt((int) xbound), rand.nextInt((int) ybound), PlayPaneSky);
         bomb1 = new Bomb(rand.nextInt((int) xbound), rand.nextInt((int) ybound), PlayPaneSky);
@@ -112,114 +107,98 @@ public class PlayScreenController {
 
         StartScreenJFX.scene.addEventHandler(KeyEvent.KEY_PRESSED, this::KeyPressed);
         speed = StartScreenController.GetSpeed();
-        timeline = new Timeline(
-                new KeyFrame(Duration.seconds(0.03), e -> {
-                    move();
-                    if(newfood){
-                        AddSnakeBody();
-                    }
-                    for(int i =0; i < snakebody.size(); i++){
-                        moveSnakeBody(snakebody.get(Integer.toString(i)), i+1);
-                    }
+        timeline = new Timeline(new KeyFrame(Duration.seconds(0.03), e -> {
+            move();
+            if(newfood){
+                AddSnakeBody();
+            }
+            for(int i =0; i < snakebody.size(); i++){
+                moveSnakeBody(snakebody.get(Integer.toString(i)), i+1);
+            }
+            try {
+                outofBounds();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            intersects = CheckBounds(food.food);
+            if (intersects) {
+                score+=521;
+                food.MoveFood();
+                newfood = true;
+            }
+            intersects = CheckBounds(wall.wall);
+            if (intersects) {
+                wall.removeWall();
+                try {
+                    RemoveSnakeBody();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            for(int i =1; i < snakebody.size(); i++){
+                intersects = CheckBounds(snakebody.get(Integer.toString(i)));
+                if(intersects){
                     try {
-                        outofBounds();
+                        ToEndScreen();
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
-                    Bounds foodbounds = food.food.localToScene(food.food.getBoundsInLocal());
-                    Bounds snakebounds = snakehead.sceneToLocal(foodbounds);
-                    if (snakehead.intersects(snakebounds)) {
-                        score+=521;
-                        food.MoveFood();
-                        newfood = true;
-                    }
-
-                    Bounds wallbound = wall.wall.localToScene(wall.wall.getBoundsInLocal());
-                    Bounds snakebound = snakehead.sceneToLocal(wallbound);
-                    if (snakehead.intersects(snakebound)) {
-                        wall.removeWall();
-                        try {
-                            RemoveSnakeBody();
-                            wallhit = true;
-                        } catch (IOException ex) {
-                            throw new RuntimeException(ex);
-                        }
-                    }
-
-                    for(int i =1; i < snakebody.size(); i++){
-                        Bounds sb = snakebody.get(Integer.toString(i)).getBoundsInLocal();
-                        Bounds sb2 = snakebody.get(Integer.toString(i)).localToScene(sb);
-                        if(snakehead.intersects(snakehead.sceneToLocal(sb2))){
-                            try {
-                                ToEndScreen();
-                            } catch (IOException ex) {
-                                throw new RuntimeException(ex);
-                            }
-                        }
-                    }
-                        try {
-                            CheckBomb(bomb.bomb);
-                            CheckBomb(bomb1.bomb);
-                            CheckBomb(bomb2.bomb);
-                        } catch (IOException ex) {
-                            throw new RuntimeException(ex);
-                        }
-                    sclabnum.setText(Integer.toString(score));
-                    if(direction == 0){
-                        SetPositions(snakehead.getLayoutX()-1,snakehead.getLayoutY()+2);
-                    } else if (direction == 1) {
-                        SetPositions(snakehead.getLayoutX()-2,snakehead.getLayoutY()-3);
-                    } else if (direction == 2) {
-                        SetPositions(snakehead.getLayoutX()-1,snakehead.getLayoutY());
-                    } else if (direction == 3) {
-                        SetPositions(snakehead.getLayoutX()-4,snakehead.getLayoutY());
-                    }
-                    gameticks++;
-                })
-
-        );
+                }
+            }
+            try {
+                CheckBomb(bomb.bomb);
+                CheckBomb(bomb1.bomb);
+                CheckBomb(bomb2.bomb);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            sclabnum.setText(Integer.toString(score));
+            if(direction == 0){
+                SetPositions(snakehead.getLayoutX()-1,snakehead.getLayoutY()+2);
+            } else if (direction == 1) {
+                SetPositions(snakehead.getLayoutX()-2,snakehead.getLayoutY()-3);
+            } else if (direction == 2) {
+                SetPositions(snakehead.getLayoutX()-1,snakehead.getLayoutY());
+            } else if (direction == 3) {
+                SetPositions(snakehead.getLayoutX()-4,snakehead.getLayoutY());
+            }
+            gameticks++;
+        }));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
         food = new Food(rand.nextInt((int)xbound),rand.nextInt((int)ybound), PlayPaneSky);
 
-        bombspawntl = new Timeline(
-                new KeyFrame(Duration.seconds(bombspawn), e -> {
-                    if(isbombs){
-                        bomb.BombSpawn();
-                        if(difficulty == 2)
-                            bomb1.BombSpawn();
-                        else if(difficulty == 3){
-                            bomb1.BombSpawn();
-                            bomb2.BombSpawn();
-                        }
-
-                    }
-                })
-            );
-            bombspawntl.setCycleCount(Timeline.INDEFINITE);
-            bombspawntl.play();
-
-            bombdonetl = new Timeline(
-                    new KeyFrame(Duration.seconds(15), e -> {
-                        bomb.BombEnd();
-                        bomb1.BombEnd();
-                        bomb2.BombEnd();
-                        bombspawntl.play();
-                        bombdonetl.stop();
-                    })
-            );
-            bombdonetl.setCycleCount(Timeline.INDEFINITE);
-
-        walltl = new Timeline(
-                new KeyFrame(Duration.seconds(20), e -> {
-                    if(hit != 0){
-                        wall = new Wall(rand.nextInt((int) xbound), rand.nextInt((int) ybound), PlayPaneSky);
-                    } else
-                        wall.moveWall();
-                    hit = 0;
-                    wallticks++;
-                })
+        bombspawntl = new Timeline(new KeyFrame(Duration.seconds(bombspawn), e -> {
+            if(isbombs){
+                bomb.BombSpawn();
+                if(difficulty == 2)
+                    bomb1.BombSpawn();
+                else if(difficulty == 3){
+                    bomb1.BombSpawn();
+                    bomb2.BombSpawn();
+                }
+            }
+        })
         );
+        bombspawntl.setCycleCount(Timeline.INDEFINITE);
+        bombspawntl.play();
+        bombdonetl = new Timeline(new KeyFrame(Duration.seconds(15), e -> {
+            bomb.BombEnd();
+            bomb1.BombEnd();
+            bomb2.BombEnd();
+            bombspawntl.play();
+            bombdonetl.stop();
+        })
+        );
+        bombdonetl.setCycleCount(Timeline.INDEFINITE);
+        walltl = new Timeline(new KeyFrame(Duration.seconds(8), e -> {
+            if(hit){
+                wall = new Wall(rand.nextInt((int) xbound), rand.nextInt((int) ybound), PlayPaneSky);
+            } else
+                wall.moveWall();
+            hit = false;
+            wallticks++;
+        }));
         walltl.setCycleCount(Timeline.INDEFINITE);
         walltl.play();
         }
@@ -229,25 +208,40 @@ public class PlayScreenController {
         ypositions.put(gameticks, y);
     }
 
+    public void SetVariables(double x, double y, int b){
+        bombspawn = b;
+        xbound = x;
+        ybound = y;
+    }
+
     public void CheckBomb(ImageView thebomb) throws IOException {
         if(thebomb.isVisible()){
-            Bounds bombbound = thebomb.localToScene(thebomb.getBoundsInLocal());
-            Bounds snakebound = snakehead.sceneToLocal(bombbound);
-            if(snakehead.intersects(snakebound)){
+            intersects = CheckBounds(thebomb);
+            if(intersects){
                 ToEndScreen();
             }
         }
     }
 
     public void RemoveSnakeBody() throws IOException {
-        if(hit == 0){
+        if(!hit){
             if(snakebody.size() == 0)
                 ToEndScreen();
             PlayPaneSky.getChildren().remove(snakebody.get(Integer.toString(snakebody.size()-1)));
             snakebody.remove(Integer.toString(snakebody.size()-1));
             score -= 521;
         }
-        hit = 1;
+        hit = true;
+    }
+
+    public boolean CheckBounds(Node n){
+        Bounds bound = n.getBoundsInLocal();
+        Bounds snakebound = n.localToScene(bound);
+        if(snakehead.intersects(snakehead.sceneToLocal(snakebound))){
+            return true;
+        }
+        else
+            return false;
     }
 
     public void AddSnakeBody(){
